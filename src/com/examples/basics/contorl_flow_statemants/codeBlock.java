b84c0f192229ff13a2308066bce6c20df533ed86
Macbook <macbook@MacBook-Air-Artur.local>
2022-04-07 12:16:37 +0200

    removetest

--- src/com/examples/basics/contorl_flow_statemants/codeBlock.java
+++ src/com/examples/basics/contorl_flow_statemants/codeBlock.java
@@ -2,9 +2,7 @@ package com.examples.basics.contorl_flow_statemants;
 
 public class codeBlock {
     public static void main(String[] args) {
-            int a = 5;
-
-            kjhbjhhb;
-
+        int a = 5;
+        System.out.println(a);
     }
 }
